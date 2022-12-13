4842e844e0c18c67e0657ed689bfb7b3286ec65e
Yeongseong Go <yeongo@c1r3s4.42seoul.kr>
2022-12-13 15:44:32 +0900

    modified : allocate head_node in ft_lstmap #2

--- src/ft_lstmap.c
+++ src/ft_lstmap.c
@@ -6,11 +6,12 @@
 /*   By: yeongo <yeongo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/12 14:15:37 by yeongo            #+#    #+#             */
-/*   Updated: 2022/12/09 18:45:40 by yeongo           ###   ########.fr       */
+/*   Updated: 2022/12/13 15:41:52 by yeongo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/libft.h"
+#include <stdlib.h>
 
 t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
@@ -20,16 +21,19 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 
 	if (lst == NULL)
 		return (NULL);
-	head_node->next = new_lst;
+	new_lst = malloc(sizeof(t_list));
+	if (new_lst == NULL)
+		return (NULL);
+	head_node = new_lst;
 	while (lst != NULL)
 	{
 		new_data = f(lst->content);
-		new_lst = ft_lstnew(new_data);
-		if (new_lst == NULL)
+		new_lst->next = ft_lstnew(new_data);
+		if (new_lst->next == NULL)
 		{
 			if (del != NULL)
 				del(new_data);
-			ft_lstclear(&new_lst, del);
+			ft_lstclear(&head_node, del);
 			return (NULL);
 		}
 		lst = lst->next;
