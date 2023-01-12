35822cc2a779c949c52680f54f172b50cbee2e43
Yeongseong Go <yeongo@c1r2s5.42seoul.kr>
2023-01-12 17:59:16 +0900

    modified : fix invalid memory access in ft_free #7

--- src/ft_free.c
+++ src/ft_free.c
@@ -6,7 +6,7 @@
 /*   By: yeongo <yeongo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 22:06:44 by yeongo            #+#    #+#             */
-/*   Updated: 2023/01/12 16:52:00 by yeongo           ###   ########.fr       */
+/*   Updated: 2023/01/12 17:35:43 by yeongo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,10 @@ void	ft_free_char(char ***ptr)
 	int	index;
 
 	index = 0;
-	while (*ptr[index] != NULL)
+	while ((*ptr)[index] != NULL)
 	{
-		free(*ptr[index]);
-		*ptr[index] = NULL;
+		free((*ptr)[index]);
+		(*ptr)[index] = NULL;
 		index++;
 	}
 	free(*ptr);
@@ -37,9 +37,9 @@ void	ft_free_void(void ***ptr, int index_max)
 {
 	while (--index_max >= 0)
 	{
-		if (*ptr[index_max] != NULL)
-			free(*ptr[index_max]);
-		*ptr[index_max] = NULL;
+		if ((*ptr)[index_max] != NULL)
+			free((*ptr)[index_max]);
+		(*ptr)[index_max] = NULL;
 	}
 	free(*ptr);
 	*ptr = NULL;
