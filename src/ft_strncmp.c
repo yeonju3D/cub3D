639a219ae377ca5b95603daa5b0f1b6388fd9497
Yeongseong Go <yeongo@c1r2s1.42seoul.kr>
2023-02-01 20:55:52 +0900

    modified : fix feature input ctrl-D in heredoc #7

--- src/ft_strncmp.c
+++ src/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: yeongo <yeongo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/10 20:36:27 by yeongo            #+#    #+#             */
-/*   Updated: 2023/02/01 19:35:38 by yeongo           ###   ########.fr       */
+/*   Updated: 2023/02/01 20:52:54 by yeongo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_strncmp(const char *s1, const char *s2, size_t n)
 	const unsigned char	*tmp_s1 = (const unsigned char *)s1;
 	const unsigned char	*tmp_s2 = (const unsigned char *)s2;
 
+	if (n == 0)
+		return (0);
 	while (*tmp_s1 && *tmp_s2 && *tmp_s1 == *tmp_s2 && n--)
 	{
 		tmp_s1++;
