eade6096906ee30275b031b40d9f1f439faa5a48
Yeongseong Go <yeongo@c1r2s1.42seoul.kr>
2023-02-03 22:26:06 +0900

    modified : include library in ft_memory functions #16

--- include/ft_memory.h
+++ include/ft_memory.h
@@ -6,14 +6,14 @@
 /*   By: yeongo <yeongo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/28 20:39:55 by yeongo            #+#    #+#             */
-/*   Updated: 2023/02/03 18:00:23 by yeongo           ###   ########.fr       */
+/*   Updated: 2023/02/03 21:22:04 by yeongo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FT_MEMORY_H
 # define FT_MEMORY_H
 
-# include <stddef.h>
+# include <stdlib.h>
 
 void	*ft_calloc(size_t count, size_t size);
 void	ft_free_str(char **str);
--- src/ft_free.c
+++ src/ft_free.c
@@ -6,11 +6,11 @@
 /*   By: yeongo <yeongo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/05 22:06:44 by yeongo            #+#    #+#             */
-/*   Updated: 2023/02/03 17:25:18 by yeongo           ###   ########.fr       */
+/*   Updated: 2023/02/03 22:17:57 by yeongo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "ft_memory.h"
+#include <stdlib.h>
 
 void	ft_free_str(char **str)
 {
@@ -31,7 +31,6 @@ void	ft_free_strings(char ***strings)
 		while ((*strings)[index] != NULL)
 		{
 			free((*strings)[index]);
-			(*strings)[index] = NULL;
 			index++;
 		}
 		free(*strings);
@@ -47,7 +46,6 @@ void	ft_free_void(void ***ptr, int index_max)
 		{
 			if ((*ptr)[index_max] != NULL)
 				free((*ptr)[index_max]);
-			(*ptr)[index_max] = NULL;
 		}
 		free(*ptr);
 		*ptr = NULL;
