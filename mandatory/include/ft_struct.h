88e1ee44150de39b7626b7b0a7b24f85cb146093
yeongo <yeongo@student.42seoul.kr>
2023-06-15 23:00:14 +0900

    feat: get board and check validate #6

--- mandatory/include/ft_struct.h
+++ mandatory/include/ft_struct.h
@@ -6,7 +6,7 @@
 /*   By: yeongo <yeongo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 21:35:15 by yeongo            #+#    #+#             */
-/*   Updated: 2023/06/14 20:37:07 by yeongo           ###   ########.fr       */
+/*   Updated: 2023/06/15 22:48:19 by yeongo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 # define FT_STRUCT_H
 
 # include "ft_enum.h"
+# include <unistd.h>
 
 # define P_NAME "cub3D"
 # define WIN_HEIGHT 5120
@@ -33,6 +34,9 @@ struct s_map
 {
 	t_info	info;
 	char	**board;
+	int		board_size;
+	int		board_capacity;
+	int		max_length;
 };
 
 struct s_cub3d
--- mandatory/srcs/main.c
+++ mandatory/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: yeongo <yeongo@student.42seoul.kr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/06 21:23:18 by yeongo            #+#    #+#             */
-/*   Updated: 2023/06/14 20:52:34 by yeongo           ###   ########.fr       */
+/*   Updated: 2023/06/15 22:57:16 by yeongo           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,9 @@
 #include "ft_memory.h"
 #include "libft.h"
 #include <mlx.h>
+#include <stddef.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <sys/fcntl.h>
 
 int	is_valid_file_format(char *map_path, char *file_exp)
@@ -227,14 +229,15 @@ int	get_texture(t_cub3d *cub3d, int fd)
 	texture_count = 0;
 	while (texture_count < DIR)
 	{
-		readline = get_next_line(fd);
-		if (readline == NULL)
+		if (get_next_line(&readline, fd) == FAIL)
 		{
 			syscall_err();
 			return (FAIL);
 		}
 		else if (skip_empty_line(&readline))
 			continue ;
+		if (readline == NULL)
+			return (FAIL);
 		texture = ft_split_set(readline, " ,\n");
 		if (!set_info_texture(cub3d, info, texture))
 			return (FAIL);
@@ -243,7 +246,151 @@ int	get_texture(t_cub3d *cub3d, int fd)
 	return (check_all_textures(info));
 }
 
-int	get_board(t_map *map, int fd);
+int	trim_new_line(char **readline)
+{
+	char	*trimmed_line;
+	size_t	length;
+
+	length = ft_strlen(*readline);
+	if (length != 0 && *readline[length - 1] == '\n')
+	{
+		trimmed_line = malloc(sizeof(char) * (length - 1));
+		if (trimmed_line == NULL)
+			return (FAIL);
+		ft_memmove(trimmed_line, *readline, length - 1);
+		trimmed_line[length - 1] = '\0';
+		free(*readline);
+		*readline = trimmed_line;
+	}
+	return (SUCCESS);
+}
+
+int	append_to_board(t_map *map, char *readline)
+{
+	char	**tmp_board;
+	int		line_length;
+
+	if (map->board_size == map->board_capacity)
+	{
+		tmp_board = malloc(sizeof(char *) * (map->board_size * 2));
+		if (tmp_board == NULL)
+			return (FAIL);
+		ft_memmove(tmp_board, map->board, map->board_size);
+		free(map->board);
+		map->board = tmp_board;
+		map->board_capacity *= 2;
+	}
+	if (!trim_new_line(&readline))
+		return (FAIL);
+	map->board[map->board_size++] = readline;
+	line_length = ft_strlen(readline);
+	if (map->max_length < line_length)
+		map->max_length = line_length;
+	return (SUCCESS);
+}
+
+int	trim_board_margine(t_map *map)
+{
+	char	**result;
+
+	if (map->board_size == 0)
+		return (FAIL);
+	result = malloc(sizeof(char *) * (map->board_size + 1));
+	if (result == NULL)
+		return (FAIL);
+	ft_memmove(result, map->board, map->board_size);
+	result[map->board_size] = NULL;
+	free(map->board);
+	map->board = result;
+	return (SUCCESS);
+}
+
+int	expand_line_length(t_map *map)
+{
+	int		index;
+	int		length;
+	char	*expand_line;
+
+	index = 0;
+	while (map->board[index])
+	{
+		length = ft_strlen(map->board[index]);
+		if (length != map->max_length)
+		{
+			expand_line = malloc(sizeof(char) * (map->max_length + 1));
+			if (expand_line == NULL)
+				return (0);
+			ft_memmove(expand_line, map->board[index], length);
+			ft_memset(expand_line + length, ' ', map->max_length - length);
+			expand_line[map->max_length] = '\0';
+		}
+		index++;
+	}
+	return (1);
+}
+
+int	in_boundary(int index_y, int index_x, t_map *map)
+{
+	if (index_y == 0 || index_y == map->board_size - 1 \
+	|| index_x == 0 || index_x == map->max_length)
+		return (1);
+	return (0);
+}
+
+int	is_valid_board(t_map *map)
+{
+	int	index_y;
+	int	index_x;
+	int	player_count;
+
+	player_count = 0;
+	index_y = 0;
+	while (map->board[index_y])
+	{
+		index_x = 0;
+		while (map->board[index_y][index_x])
+		{
+			if (map->board[index_y][index_x] == '0' \
+				|| (ft_issep(map->board[index_y][index_x], "NSWE") \
+					&& ++player_count))
+			{
+				if (in_boundary(index_y, index_x, map))
+					return (FAIL);
+				// 상하좌우 벽땅플 아니면 페일
+			}
+			index_x++;
+		}
+		index_y++;
+	}
+	return (SUCCESS);
+}
+
+int	get_board(t_map *map, int fd)
+{
+	char	*readline;
+	int		result;
+
+	while (1)
+	{
+		result = get_next_line(&readline, fd);
+		if (result == -1)
+			return (FAIL);
+		else if (readline == NULL)
+			break ;
+		if (append_to_board(map, readline) == FAIL)
+		{
+			free(readline);
+			return (FAIL);
+		}
+		free(readline);
+	}
+	if (trim_board_margine(map) == FAIL)
+		return (FAIL);
+	expand_line_length(map);
+	if (!is_valid_board(map))
+		return (FAIL);
+	return (SUCCESS);
+}
 
 int	parse_map(t_cub3d *cub3d, char *map_path)
 {
